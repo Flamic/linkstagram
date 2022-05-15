6e62a2078bb357ccd8fa26119209852430ce3413
Eugene <eugenelu.corp@gmail.com>
2022-05-15 22:16:14 +0300

    feat: added logIn and signUp routes

--- src/ui/boot/router/index.tsx
+++ src/ui/boot/router/index.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import LogInPage from 'ui/pages/logIn'
+import SignUpPage from 'ui/pages/signUp'
+
 import HomePage from '../../pages/home'
 import ProfilePage from '../../pages/profile'
 
@@ -7,6 +10,8 @@ const Router: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/logIn" element={<LogInPage />} />
+      <Route path="/signUp" element={<SignUpPage />} />
       <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   </BrowserRouter>
