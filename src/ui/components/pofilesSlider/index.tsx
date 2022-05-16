a52c8adadbfdfbeca76008412a128ce7ba52d201
Eugene <eugenelu.corp@gmail.com>
2022-05-16 15:30:16 +0300

    feat: added profiles slider

---
+++ src/ui/components/pofilesSlider/index.tsx
@@ -0,0 +1,40 @@
+import { useMediaQuery } from 'react-responsive'
+import Slider from 'react-slick'
+
+import { phoneMedia } from 'core/constants/media'
+import { Profile } from 'core/types/user'
+import Avatar from 'ui/components/common/avatar'
+
+import styles from './styles.module.scss'
+
+interface Props {
+  profiles: Profile[]
+}
+
+const ProfilesSlider: React.FC<Props> = ({ profiles }) => {
+  const matchedPhone = useMediaQuery(phoneMedia)
+
+  return (
+    <div className={styles.box}>
+      <Slider
+        centerPadding="24px"
+        swipeToSlide
+        arrows={false}
+        infinite={false}
+        variableWidth
+      >
+        {profiles.map((profile) => (
+          <Avatar
+            size={matchedPhone ? 'medium' : 'big'}
+            src={profile.profilePhotoUrl}
+            bordered
+            key={profile.username}
+          />
+        ))}
+      </Slider>
+      <div className={styles.endFilter} />
+    </div>
+  )
+}
+
+export default ProfilesSlider
