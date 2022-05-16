4d4b753824d74702c3db67ea20081f18ad2a00ac
Eugene <eugenelu.corp@gmail.com>
2022-05-16 15:37:25 +0300

    fix: added slick css styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import './ui/style/index.scss'
 
 ReactDOM.render(
