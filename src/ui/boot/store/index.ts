304ab37f728d41ce4b2d5ec32b4fdf9dbab49343
Eugene <eugenelu.corp@gmail.com>
2022-05-16 18:23:00 +0300

    fix: added RTK Query API middleware

--- src/ui/boot/store/index.ts
+++ src/ui/boot/store/index.ts
@@ -5,6 +5,8 @@ import api from 'core/store'
 export const setupStore = () =>
   configureStore({
     reducer: { [api.reducerPath]: api.reducer },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
   })
 
 export default setupStore
