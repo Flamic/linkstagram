166130f3f24aa735e088d3c44778b8b45dad0db6
Eugene <eugenelu.corp@gmail.com>
2022-05-16 15:19:02 +0300

    feat: added default avatar

--- src/ui/components/common/avatar/index.tsx
+++ src/ui/components/common/avatar/index.tsx
@@ -1,6 +1,8 @@
 import cn from 'classnames'
+import { useState } from 'react'
 
 import { ReactComponent as FollowIcon } from 'assets/images/follow-icon.svg'
+import { ReactComponent as UserIcon } from 'assets/images/user-icon.svg'
 
 import styles from './styles.module.scss'
 
@@ -8,7 +10,7 @@ interface Props {
   bordered?: boolean
   followable?: boolean
   size?: 'small' | 'medium' | 'big' | 'large'
-  src: string
+  src?: string | null
 }
 
 const Avatar: React.FC<Props> = ({
@@ -17,6 +19,8 @@ const Avatar: React.FC<Props> = ({
   src,
   size = 'medium',
 }) => {
+  const [loaded, setLoaded] = useState(false)
+
   return (
     <div
       className={cn(styles.wrapper, styles[size], {
@@ -24,7 +28,21 @@ const Avatar: React.FC<Props> = ({
         [styles.followable]: followable,
       })}
     >
-      <img src={src} alt="avatar" />
+      {src && (
+        <img
+          src={src}
+          alt="avatar"
+          draggable={false}
+          className={cn(styles.img, { [styles.invisible]: !loaded })}
+          onLoad={() => setLoaded(true)}
+          onError={() => setLoaded(false)}
+        />
+      )}
+      {!loaded && (
+        <div className={cn(styles.img, styles.userIconBox)}>
+          <UserIcon className={styles.userIcon} />
+        </div>
+      )}
       {followable && <FollowIcon className={styles.followIcon} />}
     </div>
   )
