94c52247e1f65c5458447082521095ffd0b2056b
Eugene <eugenelu.corp@gmail.com>
2022-05-17 13:33:17 +0300

    feat: added choosing & loading image for avatar

--- src/ui/components/common/avatar/index.tsx
+++ src/ui/components/common/avatar/index.tsx
@@ -9,6 +9,7 @@ import styles from './styles.module.scss'
 interface Props {
   bordered?: boolean
   followable?: boolean
+  onChoose?: FileCallback
   size?: 'small' | 'medium' | 'big' | 'large'
   src?: string | null
 }
@@ -16,21 +17,25 @@ interface Props {
 const Avatar: React.FC<Props> = ({
   bordered,
   followable,
-  src,
+  onChoose,
   size = 'medium',
+  src,
 }) => {
+  const [url, setUrl] = useState(src)
   const [loaded, setLoaded] = useState(false)
 
-  return (
+  const mainImage = (
     <div
-      className={cn(styles.wrapper, styles[size], {
-        [styles.bordered]: bordered,
-        [styles.followable]: followable,
+      className={cn(styles.wrapper, {
+        [styles[size]]: !onChoose,
+        [styles.bordered]: bordered && !onChoose,
+        [styles.followable]: followable && !onChoose,
+        [styles.choose]: onChoose,
       })}
     >
-      {src && (
+      {url && (
         <img
-          src={src}
+          src={url || undefined}
           alt="avatar"
           draggable={false}
           className={cn(styles.img, { [styles.invisible]: !loaded })}
@@ -44,6 +49,30 @@ const Avatar: React.FC<Props> = ({
         </div>
       )}
       {followable && <FollowIcon className={styles.followIcon} />}
+      {onChoose && <div className={styles.chooseLabel}>Choose new photo</div>}
+    </div>
+  )
+
+  return (
+    <div>
+      {onChoose ? (
+        <label htmlFor="selectAvatar">
+          {mainImage}
+          <input
+            type="file"
+            id="selectAvatar"
+            accept="image/png, image/jpeg"
+            style={{ display: 'none' }}
+            onChange={(event) =>
+              event.currentTarget.files?.[0] &&
+              onChoose(event.currentTarget.files?.[0]) &&
+              setUrl(URL.createObjectURL(event.currentTarget.files[0]))
+            }
+          />
+        </label>
+      ) : (
+        mainImage
+      )}
     </div>
   )
 }
