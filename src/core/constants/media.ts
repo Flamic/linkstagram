42cd1ad6de7970e05d2f59d5338394689742a2aa
Eugene <eugenelu.corp@gmail.com>
2022-05-21 17:16:15 +0300

    feat: added tablet media

--- src/core/constants/media.ts
+++ src/core/constants/media.ts
@@ -1,3 +1,4 @@
 import { MediaQuerySettings } from 'react-responsive'
 
 export const PHONE_MEDIA: MediaQuerySettings = { maxWidth: 475 }
+export const TABLET_MEDIA: MediaQuerySettings = { maxWidth: 720 }
