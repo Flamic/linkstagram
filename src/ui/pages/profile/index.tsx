55e964d564f49fdc79b85cf2297801620d96f421
Eugene <eugenelu.corp@gmail.com>
2022-05-21 03:13:10 +0300

    feat: added post view on user's page

--- src/ui/pages/profile/index.tsx
+++ src/ui/pages/profile/index.tsx
@@ -1,9 +1,12 @@
+import { useMediaQuery } from 'react-responsive'
 import { useNavigate, useParams } from 'react-router-dom'
 
+import { PHONE_MEDIA } from 'core/constants/media'
 import Header from 'ui/components/header'
 import ProfileView from 'ui/views/profile'
 
 const ProfilePage: React.FC = () => {
+  const isPhone = useMediaQuery(PHONE_MEDIA)
   const { username } = useParams()
   const navigate = useNavigate()
 
@@ -15,7 +18,7 @@ const ProfilePage: React.FC = () => {
 
   return (
     <>
-      <Header />
+      <Header mode={isPhone ? 'home' : undefined} />
       <ProfileView username={username} />
     </>
   )
--- src/ui/views/profile/index.tsx
+++ src/ui/views/profile/index.tsx
@@ -1,10 +1,15 @@
 import cn from 'classnames'
+import { useState, useEffect } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
+import { useAuth } from 'core/services/auth'
 import api from 'core/store'
+import { Post } from 'core/types/post'
 import Loader from 'ui/components/common/loader'
 import ImagesGrid from 'ui/components/imagesGrid'
 import ProfileData from 'ui/components/profileData'
+
+import PostView from '../post'
 
 import styles from './styles.module.scss'
 
@@ -14,63 +19,79 @@ interface Props {
 
 const ProfileView: React.FC<Props> = ({ username }) => {
   const isMobile = useMediaQuery({ maxWidth: 720 })
+  const currentUser = useAuth()
+  const [openedPost, setOpenedPost] = useState<Post | null>(null)
+  const [isModalOpened, setIsModalOpened] = useState(false)
+
+  const openPost = (post: Post) => {
+    setOpenedPost(post)
+    setIsModalOpened(true)
+  }
 
   const {
     data: profile,
-    isFetching: isFetchingProfile,
+    isLoading: isLoadingProfile,
     isError: isProfileError,
-    error: profileError,
   } = api.useGetProfileQuery(username)
   const {
     data: posts,
-    isFetching: isFetchingPosts,
+    isLoading: isLoadingPosts,
     isError: isPostsError,
-    error: postsError,
   } = api.useGetPostsOfUserQuery(username)
 
+  useEffect(() => setIsModalOpened(false), [profile?.username])
+
   const getProfileView = () => {
-    if (isFetchingProfile) return <Loader />
+    if (isLoadingProfile) return <Loader />
 
     if (isProfileError || !profile)
-      return (
-        <h1 className={styles.error}>
-          {profileError && 'data' in profileError
-            ? JSON.stringify(profileError.data)
-            : 'Cannot get this profile :('}
-        </h1>
-      )
+      return <h1 className={styles.error}>Cannot get this profile :(</h1>
 
     return (
       <ProfileData
         className={styles.profile}
         expanded={!isMobile}
         profile={profile}
+        onEdit={
+          profile.username === currentUser?.username ? () => 1 : undefined // TODO: Edit form
+        }
+        onPostCreate={
+          profile.username === currentUser?.username ? () => 2 : undefined // TODO: Post create form
+        }
       />
     )
   }
 
   const getPostsView = () => {
-    if (isFetchingPosts) return null
+    if (isLoadingPosts) return null
 
     if (isPostsError || !posts) {
-      if (isFetchingProfile || isProfileError || !profile) return null
+      if (isProfileError || !profile) return null
 
-      return (
-        <h1 className={styles.error}>
-          {postsError && 'data' in postsError
-            ? JSON.stringify(postsError.data)
-            : 'Cannot get posts of this user :('}
-        </h1>
-      )
+      return <h1 className={styles.error}>Cannot get posts of this user :(</h1>
     }
 
-    return <ImagesGrid images={posts.map((post) => post.photos[0])} />
+    return (
+      <ImagesGrid
+        images={posts.map((post) => ({
+          ...post.photos[0],
+          onClick: () => openPost(post),
+        }))}
+      />
+    )
   }
 
   return (
     <main className={cn(styles.main, { [styles.mobile]: isMobile })}>
       {getProfileView()}
       {getPostsView()}
+      {openedPost && (
+        <PostView
+          post={openedPost}
+          show={isModalOpened}
+          onClose={() => setIsModalOpened(false)}
+        />
+      )}
     </main>
   )
 }
