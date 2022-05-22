96ff2d66ced0ac6e3e81317f3b593eb4e6436cbc
Eugene <eugenelu.corp@gmail.com>
2022-05-22 23:29:21 +0300

    refactoring: moved modals control to ProfileData

--- src/ui/components/profileData/index.tsx
+++ src/ui/components/profileData/index.tsx
@@ -1,8 +1,12 @@
 import cn from 'classnames'
+import { useState } from 'react'
 
+import { useAuth } from 'core/services/auth'
 import { Profile } from 'core/types/user'
 import { stringifyNumber } from 'core/utils/numberConverter'
 import { getProfileName } from 'core/utils/profile'
+import CreatePostView from 'ui/views/createPost'
+import EditProfileView from 'ui/views/editProfile'
 
 import Avatar from '../common/avatar'
 import Button from '../common/button'
@@ -13,109 +17,126 @@ interface Props {
   className?: string
   expanded?: boolean
   profile: Profile
-  onEdit?(): void
-  onPostCreate?(): void
 }
 
-const ProfileData: React.FC<Props> = ({
-  className,
-  expanded,
-  profile,
-  onEdit,
-  onPostCreate,
-}) =>
-  expanded ? (
-    <section className={cn(styles.expandedBox, className)}>
-      <div className={styles.avatar}>
-        <Avatar src={profile.profilePhotoUrl} size="xl" bordered />
-      </div>
-      <div>
-        <h1 className={styles.name}>{getProfileName(profile)}</h1>
-        <h2 className={styles.job}>{profile.jobTitle}</h2>
-      </div>
-      <p className={styles.description}>{profile.description}</p>
+const ProfileData: React.FC<Props> = ({ className, expanded, profile }) => {
+  const auth = useAuth()
+  const isOwnProfile = auth?.username === profile.username
+  const [isEditProfileModalOpened, setIsEditProfileModalOpened] =
+    useState(false)
+  const [isNewPostModalOpened, setIsNewPostModalOpened] = useState(false)
 
-      <div className={cn(styles.countBox, styles.followers)}>
-        <div className={styles.value}>{stringifyNumber(profile.followers)}</div>
-        <div className={styles.text}>Followers</div>
-      </div>
-      {onEdit && (
-        <Button
-          variant="secondary"
-          className={styles.button}
-          border="border"
-          onClick={onEdit}
-        >
-          Edit profile
-        </Button>
-      )}
+  return (
+    <>
+      {expanded ? (
+        <section className={cn(styles.expandedBox, className)}>
+          <div className={styles.avatar}>
+            <Avatar
+              src={profile.profilePhotoUrl}
+              redirectTo={`/profile/${profile.username}`}
+              size="xl"
+              bordered
+            />
+          </div>
+          <div>
+            <h1 className={styles.name}>{getProfileName(profile)}</h1>
+            <h2 className={styles.job}>{profile.jobTitle}</h2>
+          </div>
+          <p className={styles.description}>{profile.description}</p>
 
-      <div className={cn(styles.countBox, styles.following)}>
-        <div className={styles.value}>{stringifyNumber(profile.following)}</div>
-        <div className={styles.text}>Following</div>
-      </div>
-      {onPostCreate && (
-        <Button
-          variant="primary"
-          className={styles.button}
-          onClick={onPostCreate}
-        >
-          New post
-        </Button>
-      )}
-    </section>
-  ) : (
-    <section className={cn(styles.box, className)}>
-      <div className={styles.avatarRow}>
-        <div className={styles.countBox}>
-          <span className={styles.value}>
-            {stringifyNumber(profile.followers)}
-          </span>
-          <span className={styles.text}>Followers</span>
-        </div>
-        <Avatar
-          src={profile.profilePhotoUrl}
-          size="large"
-          followable
-          bordered
-        />
-        <div className={styles.countBox}>
-          <span className={styles.value}>
-            {stringifyNumber(profile.following)}
-          </span>
-          <span className={styles.text}>Following</span>
-        </div>
-      </div>
-      <div>
-        {getProfileName(profile)}
-        <span className={styles.splitter}>-</span>
-        {profile.jobTitle}
-      </div>
-      <div className={styles.descriptionRow}>{profile.description}</div>
-      {(onEdit || onPostCreate) && (
-        <div className={styles.buttonRow}>
-          {onEdit && (
+          <div className={cn(styles.countBox, styles.followers)}>
+            <div className={styles.value}>
+              {stringifyNumber(profile.followers)}
+            </div>
+            <div className={styles.text}>Followers</div>
+          </div>
+          {isOwnProfile && (
             <Button
               variant="secondary"
               className={styles.button}
               border="border"
-              onClick={onEdit}
+              onClick={() => setIsEditProfileModalOpened(true)}
             >
               Edit profile
             </Button>
           )}
-          {onPostCreate && (
+
+          <div className={cn(styles.countBox, styles.following)}>
+            <div className={styles.value}>
+              {stringifyNumber(profile.following)}
+            </div>
+            <div className={styles.text}>Following</div>
+          </div>
+          {isOwnProfile && (
             <Button
               variant="primary"
               className={styles.button}
-              onClick={onPostCreate}
+              onClick={() => setIsNewPostModalOpened(true)}
             >
               New post
             </Button>
           )}
-        </div>
-      )}
-    </section>
-  )
+        </section>
+      ) : (
+        <section className={cn(styles.box, className)}>
+          <div className={styles.avatarRow}>
+            <div className={styles.countBox}>
+              <span className={styles.value}>
+                {stringifyNumber(profile.followers)}
+              </span>
+              <span className={styles.text}>Followers</span>
+            </div>
+            <Avatar
+              src={profile.profilePhotoUrl}
+              redirectTo={`/profile/${profile.username}`}
+              size="large"
+              followable
+              bordered
+            />
+            <div className={styles.countBox}>
+              <span className={styles.value}>
+                {stringifyNumber(profile.following)}
+              </span>
+              <span className={styles.text}>Following</span>
+            </div>
+          </div>
+          <div>
+            {getProfileName(profile)}
+            <span className={styles.splitter}>-</span>
+            {profile.jobTitle}
+          </div>
+          <div className={styles.descriptionRow}>{profile.description}</div>
+          {isOwnProfile && (
+            <div className={styles.buttonRow}>
+              <Button
+                variant="secondary"
+                className={styles.button}
+                border="border"
+                onClick={() => setIsEditProfileModalOpened(true)}
+              >
+                Edit profile
+              </Button>
 
+              <Button
+                variant="primary"
+                className={styles.button}
+                onClick={() => setIsNewPostModalOpened(true)}
+              >
+                New post
+              </Button>
+            </div>
+          )}
+        </section>
+      )}
+      <EditProfileView
+        show={isEditProfileModalOpened}
+        onClose={() => setIsEditProfileModalOpened(false)}
+      />
+      <CreatePostView
+        show={isNewPostModalOpened}
+        onClose={() => setIsNewPostModalOpened(false)}
+      />
+    </>
+  )
+}
 export default ProfileData
--- src/ui/views/profile/index.tsx
+++ src/ui/views/profile/index.tsx
@@ -2,14 +2,11 @@ import cn from 'classnames'
 import { useState, useEffect } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
-import { useAuth } from 'core/services/auth'
 import api from 'core/store'
 import Loader from 'ui/components/common/loader'
 import ImagesGrid from 'ui/components/imagesGrid'
 import ProfileData from 'ui/components/profileData'
 
-import CreatePostView from '../createPost'
-import EditProfileView from '../editProfile'
 import PostView from '../post'
 
 import styles from './styles.module.scss'
@@ -20,12 +17,8 @@ interface Props {
 
 const ProfileView: React.FC<Props> = ({ username }) => {
   const isMobile = useMediaQuery({ maxWidth: 720 })
-  const currentUser = useAuth()
   const [openedPost, setOpenedPost] = useState<number | null>(null)
   const [isViewPostModalOpened, setIsViewPostModalOpened] = useState(false)
-  const [isEditProfileModalOpened, setIsEditProfileModalOpened] =
-    useState(false)
-  const [isNewPostModalOpened, setIsNewPostModalOpened] = useState(false)
 
   const openPost = (postId: number) => {
     setOpenedPost(postId)
@@ -56,16 +49,6 @@ const ProfileView: React.FC<Props> = ({ username }) => {
         className={styles.profile}
         expanded={!isMobile}
         profile={profile}
-        onEdit={
-          profile.username === currentUser?.username
-            ? () => setIsEditProfileModalOpened(true)
-            : undefined
-        }
-        onPostCreate={
-          profile.username === currentUser?.username
-            ? () => setIsNewPostModalOpened(true)
-            : undefined
-        }
       />
     )
   }
@@ -99,18 +82,6 @@ const ProfileView: React.FC<Props> = ({ username }) => {
           show={isViewPostModalOpened}
           onClose={() => setIsViewPostModalOpened(false)}
         />
-      )}
-      {profile && currentUser?.username === profile?.username && (
-        <>
-          <EditProfileView
-            show={isEditProfileModalOpened}
-            onClose={() => setIsEditProfileModalOpened(false)}
-          />
-          <CreatePostView
-            show={isNewPostModalOpened}
-            onClose={() => setIsNewPostModalOpened(false)}
-          />
-        </>
       )}
     </main>
   )
